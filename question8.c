4f50ebf5fc4c07183bbb43fa44e2fce81ed8c523
DADEBIYI <dadebiyi@tcu.edu>
2025-11-11 18:14:38 -0600

    Local and Global scope program

---
+++ question8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int varOutside =420;
+
+int main (){
+	int varInside = 5;
+
+	 printf ("Variable Outside: %d\n", varOutside);
+	printf("Variable Inside: %d\n", varInside);
+
+return 0;	
+}
+

b0b57145cd9ece8d478545ada630e9bc2859b57d
DADEBIYI <dadebiyi@tcu.edu>
2025-11-14 23:43:06 -0600

    do-while loop until 0 is entered

---
+++ question20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num, count = 0;
+    do {
+        printf("Enter number (0 to stop): ");
+        scanf("%d", &num);
+        if(num != 0) count++;
+    } while(num != 0);
+    printf("Numbers entered: %d\n", count);
+    return 0;
+}
